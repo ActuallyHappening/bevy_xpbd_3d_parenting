6b7fa2feb13d037b139c42f61523dfc13116dc8e
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-01-10 19:43:34 +0900

    todo

--- examples/rotating.rs
+++ examples/rotating.rs
@@ -1,8 +1,8 @@
 //! Shows a basic usage of [InternalForce].
 //! This spawns a center red cube, and two children on either side.
-//! 
+//!
 //! The child on the right has an internal force facing downwards, which should rotate the parent clockwise.
-//! 
+//!
 //! Note also that this system is not balanced as is clearly seen, so center of mass is taken into account
 
 use bevy::prelude::*;
@@ -10,99 +10,96 @@ use bevy_xpbd3d_parenting::InternalForce;
 use bevy_xpbd_3d::prelude::*;
 
 fn main() {
-    App::new()
-        .add_plugins((
-            DefaultPlugins,
-            PhysicsPlugins::default(),
-            bevy_xpbd3d_parenting::PhysicsParentingPlugin::default(),
-        ))
-        .add_systems(Startup, setup)
-        .run();
+	App::new()
+		.add_plugins((
+			DefaultPlugins,
+			PhysicsPlugins::default(),
+			bevy_xpbd3d_parenting::PhysicsParentingPlugin::default(),
+		))
+		.add_systems(Startup, setup)
+		.run();
 }
 
 fn setup(
-    mut commands: Commands,
-    mut meshs: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+	mut commands: Commands,
+	mut meshs: ResMut<Assets<Mesh>>,
+	mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0., 0., 10.),
-        ..default()
-    });
+	// camera
+	commands.spawn(Camera3dBundle {
+		transform: Transform::from_xyz(0., 0., 10.),
+		..default()
+	});
 
-    // ground
-    commands.spawn((
-        PbrBundle {
-            mesh: meshs.add(Mesh::from(shape::Plane {
-                size: 10.,
-                ..default()
-            })),
-            material: materials.add(Color::BLACK.into()),
-            ..default()
-        },
-        RigidBody::Static,
-    ));
-		// also, remove gravity
-		commands.insert_resource(Gravity(Vec3::ZERO));
+	// ground
+	commands.spawn((
+		PbrBundle {
+			mesh: meshs.add(Mesh::from(shape::Plane {
+				size: 10.,
+				..default()
+			})),
+			material: materials.add(Color::BLACK.into()),
+			..default()
+		},
+		RigidBody::Static,
+	));
+	// also, remove gravity
+	commands.insert_resource(Gravity(Vec3::ZERO));
 
-    // cube
-    let mut cube = commands.spawn((
-        PbrBundle {
-            mesh: meshs.add(Mesh::from(shape::Cube { size: 1. })),
-            material: materials.add(Color::RED.into()),
-            transform: Transform::from_xyz(0., 5., 0.),
-            ..default()
-        },
-        RigidBody::Dynamic,
-				
-				// IMPORTANT: parent's external force must be non-persistent
-				// so that each frame this library can update it just after it resets
-        ExternalForce::ZERO.with_persistence(false),
+	// cube
+	let mut cube = commands.spawn((
+		PbrBundle {
+			mesh: meshs.add(Mesh::from(shape::Cube { size: 1. })),
+			material: materials.add(Color::RED.into()),
+			transform: Transform::from_xyz(0., 5., 0.),
+			..default()
+		},
+		RigidBody::Dynamic,
+		// IMPORTANT: parent's external force must be non-persistent
+		// so that each frame this library can update it just after it resets
+		ExternalForce::ZERO.with_persistence(false),
+		// doesn't really matter what actual computed collider you chose
+		// you could just add manual collider as well
+		AsyncCollider(ComputedCollider::ConvexHull),
+	));
 
-        // doesn't really matter what actual computed collider you chose
-        // you could just add manual collider as well
-        AsyncCollider(ComputedCollider::ConvexHull),
-    ));
+	// cube child 1: normal child
+	cube.with_children(|cube| {
+		cube.spawn((
+			PbrBundle {
+				mesh: meshs.add(Mesh::from(shape::Cube { size: 0.5 })),
+				// lighter red child
+				material: materials.add(Color::rgb(0.5, 0.0, 0.0).into()),
+				transform: Transform::from_xyz(-3.0, 0.0, 0.0),
+				..default()
+			},
+			// no rigidbody
+			// no external force
+			// for this specific, child no internal force
+			Collider::capsule(0.5, 0.5),
+		));
+	});
 
-    // cube child 1: normal child
-    cube.with_children(|cube| {
-        cube.spawn((
-            PbrBundle {
-                mesh: meshs.add(Mesh::from(shape::Cube { size: 0.5 })),
-                // lighter red child
-                material: materials.add(Color::rgb(0.5, 0.0, 0.0).into()),
-								transform: Transform::from_xyz(-3.0, 0.0, 0.0),
-                ..default()
-            },
-            // no rigidbody
-            // no external force
-            // for this specific, child no internal force
-            Collider::capsule(0.5, 0.5),
-        ));
-    });
-
-    // sphere child 2: using internal force
-    cube.with_children(|cube| {
-        cube.spawn((
-            PbrBundle {
-                mesh: meshs.add(Mesh::from(shape::UVSphere {
-                    radius: 0.5,
-                    ..default()
-                })),
-								// blue child
-                material: materials.add(Color::BLUE.into()),
-								// to the right a bit
-								transform: Transform::from_xyz(3.0, 0.0, 0.0),
-                ..default()
-            },
-            // no rigidbody
-            // no external force
-            // for this specific, using internal force
-            AsyncCollider(ComputedCollider::ConvexHull),
-
-						// internal force pushes downwards, which should rotate clockwise
-            InternalForce(Vec3::new(0., -3., 0.)),
-        ));
-    });
+	// sphere child 2: using internal force
+	cube.with_children(|cube| {
+		cube.spawn((
+			PbrBundle {
+				mesh: meshs.add(Mesh::from(shape::UVSphere {
+					radius: 0.5,
+					..default()
+				})),
+				// blue child
+				material: materials.add(Color::BLUE.into()),
+				// to the right a bit
+				transform: Transform::from_xyz(3.0, 0.0, 0.0),
+				..default()
+			},
+			// no rigidbody
+			// no external force
+			// for this specific, using internal force
+			AsyncCollider(ComputedCollider::ConvexHull),
+			// internal force pushes downwards, which should rotate clockwise
+			InternalForce(Vec3::new(0., -3., 0.)),
+		));
+	});
 }
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,42 +1,55 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::RenderPlugin, winit::WinitPlugin};
 use bevy_xpbd3d_parenting::prelude::*;
 pub use bevy_xpbd_3d::prelude::*;
+use test_log::test;
 
-fn main() {
-    let mut app = App::new();
+#[test]
+fn assert_moves() {
+	let mut app = App::new();
 
-    // initialize plugins
-    app.add_plugins((
-        DefaultPlugins,
-        // bevy xpbd
-        bevy_xpbd_3d::prelude::PhysicsPlugins::default(),
-        // custom plugin, does not technically depend on bevy_xpbd_3d
-        // but it makes sense to add afterwards
-        bevy_xpbd3d_parenting::PhysicsParentingPlugin::default(),
-    ));
+	// initialize plugins
+	app.add_plugins((
+		MinimalPlugins,
+		// bevy xpbd
+		bevy_xpbd_3d::prelude::PhysicsPlugins::new(Update),
+		// custom plugin, does not technically depend on bevy_xpbd_3d
+		// but it makes sense to add afterwards
+		bevy_xpbd3d_parenting::PhysicsParentingPlugin::default(),
+	));
 
-    // spawn parent (example)
-    app.world
-        .spawn((
-            PbrBundle {
-                // add your own things here
-                ..default()
-            },
-            RigidBody::Dynamic,
-            // NB: Parent must have external force that is NOT persistent!
-            // Since this is not the default, add it manually
-            ExternalForce::ZERO.with_persistence(false),
-            // bevy_xpbd requires that [RigidBody]s have a collider
-            // which is used to compute mass and center of gravity
-            Collider::capsule(1.0, 1.0),
-        ))
-        .with_children(|parent| {
-            parent.spawn((
-                PbrBundle {
-                    // add your own things here
-                    ..default()
-                },
-                InternalForce(Vec3::new(0., 3., 0.)),
-            ));
-        });
+	// spawn parent (example)
+	let mut parent = app.world.spawn((
+		PbrBundle {
+			// add your own things here
+			..default()
+		},
+		RigidBody::Dynamic,
+		// NB: Parent must have external force that is NOT persistent!
+		// Since this is not the default, add it manually
+		ExternalForce::ZERO.with_persistence(false),
+		// bevy_xpbd requires that [RigidBody]s have a collider
+		// which is used to compute mass and center of gravity
+		Collider::capsule(1.0, 1.0),
+	));
+	let mut child = None;
+	parent.with_children(|parent| {
+		child = Some(
+			parent
+				.spawn((
+					PbrBundle {
+						// add your own things here
+						..default()
+					},
+					InternalForce(Vec3::new(0., 3., 0.)),
+				))
+				.id(),
+		);
+	});
+	let parent = parent.id();
+
+	app.world.run_schedule(Main);
+
+	assert!(child.is_some());
+
+	assert!(app.world.get::<Transform>(parent).unwrap().translation.y > 0.0);
 }
