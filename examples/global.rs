ea0a8d7fc8d524c0b162f17875a70fe6a7d47225
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-02-25 15:48:15 +1000

    add: example global

---
+++ examples/global.rs
@@ -0,0 +1,88 @@
+//! Demonstrates how two global [InternalForce]s can counteract each other.
+
+use bevy::prelude::*;
+use bevy_xpbd_3d::prelude::*;
+use bevy_xpbd_3d_parenting::InternalForce;
+
+fn main() {
+	App::new()
+		.add_plugins((
+			DefaultPlugins,
+			PhysicsPlugins::new(Update),
+			bevy_xpbd_3d_parenting::prelude::ParentingPlugin::new(Update),
+		))
+		.add_systems(Startup, setup)
+		.run();
+}
+
+fn setup(
+	mut commands: Commands,
+	mut meshs: ResMut<Assets<Mesh>>,
+	mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+	// camera
+	commands.spawn(Camera3dBundle {
+		transform: Transform::from_xyz(0., 0., 10.),
+		..default()
+	});
+
+	// ground
+	commands.spawn((
+		PbrBundle {
+			mesh: meshs.add(Plane3d::default()),
+			material: materials.add(Color::BLACK),
+			..default()
+		},
+		RigidBody::Static,
+	));
+	// also, remove gravity
+	commands.insert_resource(Gravity(Vec3::ZERO));
+
+	// cube
+	let mut cube = commands.spawn((
+		PbrBundle {
+			mesh: meshs.add(Cuboid::default()),
+			material: materials.add(Color::RED),
+			transform: Transform::from_xyz(0., 0., 0.),
+			..default()
+		},
+		RigidBody::Dynamic,
+		// IMPORTANT: parent's external force must be non-persistent
+		// so that each frame this library can update it just after it resets
+		ExternalForce::ZERO.with_persistence(false),
+		// doesn't really matter what actual computed collider you chose
+		// you could just add manual collider as well
+		Collider::cuboid(1.0, 1.0, 1.0),
+	));
+
+	// cube child 1: normal child
+	cube.with_children(|cube| {
+		cube.spawn((
+			PbrBundle {
+				mesh: meshs.add(Cuboid::default()),
+				// lighter red child
+				material: materials.add(Color::rgb(0.5, 0.0, 0.0)),
+				transform: Transform::from_xyz(3.0, 0.0, 0.0),
+				..default()
+			},
+			Collider::capsule(0.5, 0.5),
+			InternalForce::new_global(Vec3::new(0.0, 1.0, 0.0)),
+		));
+	});
+
+	// sphere child 2: using globinternal force
+	cube.with_children(|cube| {
+		cube.spawn((
+			PbrBundle {
+				mesh: meshs.add(Sphere::default().mesh().uv(16, 18)),
+				// blue child
+				material: materials.add(Color::BLUE),
+				// to the left a bit
+				transform: Transform::from_xyz(-3.0, 0.0, 0.0),
+				..default()
+			},
+			Collider::capsule(0.5, 0.5),
+			InternalForce::new_global(Vec3::new(0., -1.0, 0.0)),
+		));
+	});
+}
--- tests/global.rs
+++ tests/global.rs
@@ -38,11 +38,11 @@ proptest! {
 	}
 
 	#[test]
-	fn local_factors_rotation(rot1 in 0.1f32 .. 4f32, rot2 in 0.1f32 .. 4f32) {
+	fn local_factors_rotation_parent(rot1 in 0.1f32 .. 0.9f32) {
 		let mut app = test_app(None);
 
 		let mut parent = app.world.spawn((
-			TransformBundle::from_transform(Transform::from_rotation(Quat::from_rotation_z(TAU / rot1))),
+			TransformBundle::from_transform(Transform::from_rotation(Quat::from_rotation_z(TAU * rot1))),
 			RigidBody::Dynamic,
 			ExternalForce::ZERO.with_persistence(false),
 			Collider::capsule(1.0, 1.0),
@@ -51,7 +51,39 @@ proptest! {
 		// child with internal force
 		parent.with_children(|parent| {
 			parent.spawn((
-				TransformBundle::from_transform(Transform::from_rotation(Quat::from_rotation_z(TAU / rot2))),
+				TransformBundle::default(),
+				Collider::capsule(1.0, 1.0),
+				InternalForce::new_local(-Vec3::Y * 10.0),
+			));
+		});
+
+		let parent = parent.id();
+		let get_parent_transform = get::<Transform>(parent);
+
+		for _ in 0..SETUP_ITERATIONS {
+			app.update();
+		}
+
+		assert!(get_parent_transform(&mut app.world).translation.y != 0.0);
+		assert!(get_parent_transform(&mut app.world).translation.x != 0.0, "Parent transform .x != 0: {:?}", get_parent_transform(&mut app.world).translation);
+		assert!(get_parent_transform(&mut app.world).translation.z == 0.0);
+	}
+
+	#[test]
+	fn local_factors_rotation_child(rot_child in 0.1f32 .. 0.9f32) {
+		let mut app = test_app(None);
+
+		let mut parent = app.world.spawn((
+			TransformBundle::from_transform(Transform::from_rotation(Quat::from_rotation_z(0.0))),
+			RigidBody::Dynamic,
+			ExternalForce::ZERO.with_persistence(false),
+			Collider::capsule(1.0, 1.0),
+		));
+
+		// child with internal force
+		parent.with_children(|parent| {
+			parent.spawn((
+				TransformBundle::from_transform(Transform::from_rotation(Quat::from_rotation_z(TAU * rot_child))),
 				Collider::capsule(1.0, 1.0),
 				InternalForce::new_local(-Vec3::Y * 10.0),
 			));
